2bcef4f8296a488f50d4cbca251d45ebd096f5d2
Leviii <92025034+LeviiZeng@users.noreply.github.com>
2023-05-12 22:06:42 +0800

    Add files via upload

--- Dungeon-game-with-unity-Enemy/Assets-Enemy/Scripts/EnemyScript.cs
+++ Dungeon-game-with-unity-Enemy/Assets-Enemy/Scripts/EnemyScript.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+锘縰sing System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,34 +10,35 @@ public class EnemyScript : MonoBehaviour
     private bool targetCollision = false;
     private float speed = 2.0f;
     private float thrust = 1.5f;
-    public int health = 5;
+    public float health = 5;
+    private int hitStrength = 10;
 
-    //enemy死了以后变成一滩血
     public Sprite deathSprite;
     public Sprite[] sprites;
 
-    private bool isDead = false;
+    private GameManager gameManager;
 
+    private bool isDead = false;
     void Start()
     {
-        //enemy死了以后变成一滩血
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         int rnd = Random.Range(0, sprites.Length);
         GetComponent<SpriteRenderer>().sprite = sprites[rnd];
         target = GameObject.Find("Player").transform;
+        health += (0.1f * gameManager.GetLevel());
     }
 
-    // Update is called once per frame
     void Update()
     {
         range = Vector2.Distance(transform.position, target.position);
-        if (range < minDistance && !isDead)
+        if(range < minDistance && !isDead)
         {
             if (!targetCollision)
             {
-                //Get the position of the player
+                // Get the position of the player
                 transform.LookAt(target.position);
 
-                //Correct the rotation
+                // Correct the rotation
                 transform.Rotate(new Vector3(0, -90, 0), Space.Self);
                 transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
             }
@@ -45,7 +46,6 @@ public class EnemyScript : MonoBehaviour
         transform.rotation = Quaternion.identity;
     }
 
-    //如果和玩家碰撞，enemy后退一点并导致伤害
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && !targetCollision)
@@ -54,7 +54,7 @@ public class EnemyScript : MonoBehaviour
             Vector3 center = collision.collider.bounds.center;
 
             targetCollision = true;
-            //检查和center的距离
+
             bool right = contactPoint.x > center.x;
             bool left = contactPoint.x < center.x;
             bool top = contactPoint.y > center.y;
@@ -64,18 +64,17 @@ public class EnemyScript : MonoBehaviour
             if (left) GetComponent<Rigidbody2D>().AddForce(-transform.right * thrust, ForceMode2D.Impulse);
             if (top) GetComponent<Rigidbody2D>().AddForce(transform.up * thrust, ForceMode2D.Impulse);
             if (bottom) GetComponent<Rigidbody2D>().AddForce(-transform.up * thrust, ForceMode2D.Impulse);
-
             Invoke("FalseCollision", 0.5f);
         }
     }
-    //超过和玩家的范围以后停止碰撞后退，继续向玩家靠近
+
     void FalseCollision()
     {
         targetCollision = false;
         GetComponent<Rigidbody2D>().velocity = Vector3.zero;
     }
 
-    public void TakeDamage(int amount)
+    public void TakeDamage(float amount)
     {
         health -= amount;
         if(health < 0)
@@ -83,29 +82,31 @@ public class EnemyScript : MonoBehaviour
             isDead = true;
             GetComponent<Rigidbody2D>().velocity = Vector3.zero;
             GetComponent<SpriteRenderer>().sprite = deathSprite;
-            //enemy死后的血迹不会出现在活着的enemy图像上
             GetComponent<SpriteRenderer>().sortingOrder = -1;
-
             GetComponent<Collider2D>().enabled = false;
-            Destroy(transform.GetChild(0).gameObject);
-            Invoke("EnemyDeath", 5.0f);
-            
+            transform.GetChild(0).gameObject.SetActive(false);
+            target.GetComponent<PlayerScript>().GainExperience(100);
+            Invoke("EnemyDeath", 1.5f);
         } else
         {
-            //transform.GetChild(0).gameObject.SetActive(true);
-            transform.GetChild(0).gameObject.SetActive(false);
-            //Invoke("HideBlood", 0.25f);
+            transform.GetChild(0).gameObject.SetActive(true);
+            Invoke("HideBlood", 0.25f);
         }
     }
-    //被攻击才会显示blood图样
-//    void HideBlood()
-//    {
-//        transform.GetChild(0).gameObject.SetActive(false);
-//    }
 
-    //死了以后enemy图像消失
+    void HideBlood()
+    {
+        transform.GetChild(0).gameObject.SetActive(false);
+    }
+
     void EnemyDeath()
     {
+        gameManager.SetZombieCount(-1);
         Destroy(gameObject);
+    }
+
+    public int GetHitStrength()
+    {
+        return hitStrength;
     }
 }
